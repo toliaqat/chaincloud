63dfb97d041f8036e0ad559b3c1966d1fe789372
Touseef Liaqat <touseefliaqat@gmail.com>
2023-11-13 00:45:41 -0800

    Add APIs for agd, test keys and logs

---
+++ README.md
@@ -0,0 +1,29 @@
+# Docker Management API
+
+## Project Overview
+This is a proof of concept project that provides basic functionalities to manage docker containers of agoric blockchain.
+
+> :warning: **Notice**: This is a proof of concept project and should not be used in production. The API currently lacks robust security measures and is susceptible to abuse.
+
+## Getting Started
+
+### Prerequisites
+- Node.js
+- Docker
+
+### Installation
+2. yarn install
+3. node server.js
+
+
+## Security
+This project is intended for demonstration purposes and does not include comprehensive security features. It is not recommended for use in a production environment.
+
+## Contributing
+Contributions are welcome. Please adhere to the following steps:
+- Fork the repository.
+- Create a new branch from `main`.
+- Submit a pull request with your changes.
+
+
+
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const https = require('https');
+const fs = require('fs');
 const express = require('express');
-const { exec } = require('child_process');
+const { exec, execFile } = require('child_process');
 const app = express();
-const port = 3001;
+const port = 443;
 
 function generateUniqueName() {
     const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
@@ -11,6 +13,11 @@ function generateUniqueName() {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
+}
+
+function isValidContainerName(input) {
+    const validInputRegex = /^[a-zA-Z0-9]+$/;
+    return validInputRegex.test(input);
 }
 
 function checkRunningContainers(callback) {
@@ -25,7 +32,8 @@ function checkRunningContainers(callback) {
     });
 }
 
-app.get('/start-chain', (req, res) => {
+// GET /chain
+app.get('/chain', (req, res) => {
     console.log('Received request to start a new container');
 
     checkRunningContainers((err, count) => {
@@ -43,17 +51,99 @@ app.get('/start-chain', (req, res) => {
 
         const uniqueName = generateUniqueName();
 
-        exec(`CONTAINER_NAME=${uniqueName} docker-compose -p ${uniqueName} up -d`, (error, stdout, stderr) => {
+        exec(`CONTAINER_NAME=${uniqueName} docker compose -p ${uniqueName} up -d`, (error, stdout, stderr) => {
             if (error) {
                 console.error(`Error starting container ${uniqueName}:`, error);
                 return res.status(500).send(`Error starting container: ${error.message}`);
             }
 
-            console.log(`Started container ${uniqueName}. Docker Compose Output: ${stdout}`);
-            res.send(`Container started. Name: ${uniqueName}`);
+            cName = `${uniqueName}-agd-1`
+
+            console.log(`Started container ${cName}`);
+            res.json({ containerName: cName });
         });
     });
 });
+
+// GET /logs
+app.get('/logs', (req, res) => {
+    const containerName = req.query.containerName;
+    
+    if (!containerName) {
+        return res.status(400).json({ error: 'Container name is required' });
+    }
+
+    if (!isValidContainerName(containerName)) {
+        return res.status(400).json({ error: 'Invalid container name' });
+    }
+
+    exec(`docker logs -n 10 ${containerName}`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error fetching logs for container ${containerName}:`, error);
+            return res.status(500).json({ error: `Error fetching logs: ${error.message}` });
+        }
+
+        res.json({ containerName: containerName, logs: stdout });
+    });
+});
+
+
+// GET /agd
+app.get('/agd', (req, res) => {
+    
+    const containerName = req.query.containerName;
+    const agdParams = req.query.agdParams;
+
+    if (!containerName) {
+        return res.status(400).json({ error: 'Container name is required' });
+    }
+
+    if (!agdParams) {
+        return res.status(400).json({ error: 'Agd parameters are required' });
+    }
+
+    if (!isValidContainerName(containerName)) {
+        return res.status(400).json({ error: 'Invalid container name' });
+    }
+
+    execFile('docker', ['exec', containerName, 'agd'].concat(agdParams.split(' ')), (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error running agd in the agoric chain container ${containerName}:`, error);
+            return res.status(500).json({ error: `Error fetching logs: ${error.message}` });
+        }
+
+        res.json({ containerName: containerName, logs: stdout });
+    });
+});
+
+
+// GET /keys
+app.get('/keys', (req, res) => {
+    
+    const containerName = req.query.containerName;
+    if (!containerName) {
+        return res.status(400).json({ error: 'Container name is required' });
+    }
+
+    if (!isValidContainerName(containerName)) {
+        return res.status(400).json({ error: 'Invalid container name' });
+    }
+
+    exec(`docker exec ${containerName} /bin/bash -c 'source env_setup.sh && printKeys' `, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error fetching test keys from container ${containerName}:`, error);
+            return res.status(500).json({ error: `Error fetching logs: ${error.message}` });
+        }
+
+        res.json({ containerName: containerName, keys: stdout });
+    });
+});
+
+
+const options = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+};
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
